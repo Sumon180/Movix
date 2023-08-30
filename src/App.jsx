e843618f73914b825d431e8be0a5b67d3c4956be
Sumon Hossain <sumonengr.96@gmail.com>
2023-08-30 21:19:04 +0600

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ function App() {
     });
 
     const data = await Promise.all(promises);
-    console.log(data);
+    //!  console.log(data);
     data.map(({ genres }) => {
       return genres.map((item) => (allGenres[item.id] = item));
     });
